881368270b50a6f800fddec602770d441b2731ed
szuti <p.szuttenbach@appstract.software>
2018-03-19 19:36:40 +0100

    multiple phones support

--- ios/RCTContactsWrapper/RCTContactsWrapper.m
+++ ios/RCTContactsWrapper/RCTContactsWrapper.m
@@ -141,8 +141,15 @@ RCT_EXPORT_METHOD(getEmail:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseR
       
       //Return first phone number
       if([phoneNos count] > 0) {
-        CNPhoneNumber *phone = ((CNLabeledValue *)phoneNos[0]).value;
-        [contactData setValue:phone.stringValue forKey:@"phone"];
+        NSUInteger count = [phoneNos count];
+        NSArray *phones;
+        for (NSUInteger i = 0; i < count; i++) {
+          CNPhoneNumber *phone = ((CNLabeledValue *)phoneNos[i]).value;
+          phones[i] = phone;
+        }
+        
+        NSString *joinedPhone = [phones componentsJoinedByString:@","];
+        [contactData setValue:joinedPhone.stringValue forKey:@"phone"];
       }
       
       //Return first email address
